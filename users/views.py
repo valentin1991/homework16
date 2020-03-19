5e21419ffc4683892134269164d565df202b054c
valentin1991 <valentin.semenov.1991@mail.ru>
2020-03-19 13:09:48 +0300

    homework16

--- users/views.py
+++ users/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UseerOurRegistration
 
 def register(request):
     if request.method == "POST":
-            form = UserCreationForm(request.POST)
+            form = UseerOurRegistration(request.POST)
             if form.is_valid():
+                form.save()
                 username = form.cleaned_data.get('username')
                 messages.success(request, f'Пользователь {username} был успешно создан')
                 return redirect('main-home')
