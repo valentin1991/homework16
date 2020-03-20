730f5524d4e2d1b607cd11be0ecb1637552617df
valentin1991 <valentin.semenov.1991@mail.ru>
2020-03-20 12:30:16 +0300

    homework16

--- users/models.py
+++ users/models.py
@@ -12,7 +12,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     img = models.ImageField(default = 'default.jpg', upload_to = 'user_images')
     check_agree = models.BooleanField(default = True, verbose_name = "Согланешие об отправке уведомлений на электронную почту")
-    gender = models.CharField(max_length=3, choices=GENDER_CHOICES)
+    gender = models.CharField(max_length=3, choices=GENDER_CHOICES, verbose_name = 'Выберите пол')
 
     def __str__(self):
         return f'Профаил пользователя {self.user.username}'
